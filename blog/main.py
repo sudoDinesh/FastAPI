c3c446e2e262656b7832358692caf2e30bea11ba
Dinesh Kumar <dinesh@Zoro.local>
2025-01-25 16:00:30 +0530

    Basic CRUD operations

---
+++ blog/main.py
@@ -0,0 +1,70 @@
+from typing import List
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+from . import schemas, models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+models.Base.metadata.create_all(engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,  db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} not found')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request:schemas.Blog, db:Session = Depends(get_db)):
+     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+     if not blog:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} not found')
+     blog.update({'title':request.title, 'body': request.body}) 
+     db.commit()
+     return 'updated'
+
+
+
+@app.get('/blog', response_model = List[schemas.ShowBlog])
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model = schemas.ShowBlog)
+def all(id, response: Response,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} is not available")
+        # response.status_code = status.HTTP_404_NOT_FOUND 
+        # return {'detail': f"Blog with the id {id} is not available"}
+    return blog
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -7,3 +10,36 @@ app = FastAPI()
 @app.get('/')
 def test():
     return 'test'
+
+ 
+@app.get('/blog')
+def index(limit=10, published:bool=True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from db'}
+    else:
+        return {'data': f'{limit} blogs from db'}
+    
+
+@app.get('/blog/{id}')
+def blog(id:int):
+    return {'data':id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': [1,2]}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data' : f'blog is created with {request.title}'}
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1',port=9000)

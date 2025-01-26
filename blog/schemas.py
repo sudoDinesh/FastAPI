9b3a96f91941370a603b11a6cb15a49c91b91f33
Dinesh Kumar <dinesh@Zoro.local>
2025-01-26 18:15:33 +0530

    relationships

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,14 +1,13 @@
 from pydantic import BaseModel
+from typing import List
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
 
-class ShowBlog(BaseModel):
-    title: str
-    body: str
+class Blog(BlogBase):
     class Config():
-        orm_mode = True        
+        orm_mode = True      
 
 class User(BaseModel):
     name: str
@@ -18,6 +17,13 @@ class User(BaseModel):
 class ShowUser(BaseModel):
     name: str
     email: str
+    blogs: List[Blog] = []
     class Config():
         orm_mode = True        
 
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+    class Config():
+        orm_mode = True  

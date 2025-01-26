63fd86ef585e1e64228e517f3ff10bf943572ad8
Dinesh Kumar <dinesh@Zoro.local>
2025-01-26 17:28:26 +0530

    users

--- blog/main.py
+++ blog/main.py
@@ -69,3 +69,11 @@ def create_user(request: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user/{id}', response_model=schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with the id {id} is not available")
+    return user
